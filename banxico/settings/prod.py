4e372162bd0ee43329c5f7afccac7aa25b1041a3
Josue Tamayo <tamayo.josue@gmail.com>
2022-01-18 14:52:28 -0600

    return old prod

--- banxico/settings/prod.py
+++ banxico/settings/prod.py
@@ -5,15 +5,4 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['198.199.73.58']
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.parent / 'db.sqlite3',
-    }
-}
-
-
-STATIC_ROOT = BASE_DIR.parent / 'static'
+STATIC_ROOT = BASE_DIR.parent.parent / 'static'
