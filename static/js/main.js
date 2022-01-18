88f3f6b374cebd850a8ee7ddcf83dd1a149c1a3c
Josue Tamayo <tamayo.josue@gmail.com>
2022-01-18 13:43:56 -0600

    added style to template

--- static/js/main.js
+++ static/js/main.js
@@ -3,24 +3,6 @@ async function getData(url = '') {
   return response.json()
 }
 
-
-// function set_chart() {
-// 	google.charts.load('current', {packages: ['corechart']});  
-// 	let data = google.visualization.arrayToDataTable([
-//      ['Fecha', 'UDIS', { role: 'style' }],
-//      ['2022-12-25', 8.94],
-//      ['2022-12-24', 10.49],
-//      ['2022-12-23', 19.30],
-//   ]);
-
-// 	let options = {title: 'Population (in millions)'}; 
-
-// 	let chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-//   chart.draw(data, options);
-//   google.charts.setOnLoadCallback(drawChart);
-// }
-
-
 function get_data_banxico() {
 	let start_date = document.getElementById('start').value
 	let end_date = document.getElementById('end').value
@@ -32,11 +14,10 @@ function get_data_banxico() {
 	let max = document.getElementById('max')
 
 	if (start_date && end_date) {
-		console.log(select)
 		getData(`/api_get_data/${select}/${start_date}/${end_date}/`)
 		.then(data => {
 			if (data.status_code == 202) {
-				result.innerHTML = data.data_list
+				/*result.innerHTML = data.data_list*/
 				avg.innerHTML = data.avg_data
 				min.innerHTML = data.min_data
 				max.innerHTML = data.max_data
